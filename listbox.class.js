3e45771778300bd864b9a4e7af881905a69c12bd
Rich Caloggero <rjc@mit.edu>
2018-10-22 21:01:16 -0400

    * list navigation seems to work - need first letter navigation

--- listbox.class.js
+++ listbox.class.js
@@ -4,25 +4,41 @@ this.multiple = false;
 this._selectedIndex = -1;
 this.domNode = element;
 element.setAttribute("role", "listbox");
+element.setAttribute("style", "list-style:none;");
+
 element.addEventListener ("keydown", (e) => {
 if (this.isEmpty()) return;
 if (e.target === this.getFocus()) {
 console.log("key: ", e.key);
-this._keymap[e.key]();
+const command = this._handleKey(e.key);
+if (command) {
+command.apply(this);
 e.preventDefault();
+} // if
+
 } else {
 alert("event target and currently focused item should be the same");
 } // if
 }); // listen for keyup
 
 this._keymap = {
-"ArrowUp": () => {
-this.focus(this.focusIndex()+1);
+"ArrowDown": () => {
+this.setFocus(this.getFocusIndex()+1);
 }, // "ArrowUp"
 
-"ArrowDown": () => {
-this.focus(this.focusIndex() - 1);
+"ArrowUp": () => {
+console.log("command: ArrowDown");
+this.setFocus(this.getFocusIndex() - 1);
 }, // "ArrowDown"
+
+"Home": () => {
+this.setFocus(0);
+}, // Home
+
+"End": () => {
+console.log("command: End");
+this.setFocus(this.length-1);
+}, // Home
 
 " ": () => {
 if (this.multiple && this.getFocus()) {
@@ -50,6 +66,8 @@ isEmpty () {
 return this.children().length === 0;
 } // isEmpty
 
+get length () {return this.children().length;}
+
 isValidIndex (index) {
 return index >= 0 && index < this.children().length;
 } // isValidIndex
@@ -76,6 +94,7 @@ if (!(option instanceof HTMLElement)) option = this.createOption.apply (this, ar
 option.setAttribute("role", "option");
 option.setAttribute("tabindex", "-1");
 this.domNode.appendChild(option);
+this.setFocus(0);
 return this;
 } // add
 
@@ -127,7 +146,7 @@ return this.domNode.querySelector("[tabindex='0']");
 
 getFocusIndex () {
 return this.getFocus()? this.indexOf(this.getFocus()) : -1;
-} // focusIndex
+} // getFocusIndex
 
 unselectAll () {
 this.children().forEach (node => node.setAttribute(
@@ -163,8 +182,10 @@ return node? node.getAttribute("value") : undefined;
 
 
 _handleKey (key) {
-if (this.isEmpty()) return;
-return this._keymap[key]();
+if (!key || this.isEmpty()) return;
+const command = this._keymap[key];
+console.log ("command: ", command);
+return command? command : undefined;
 } // handleKey
 
 } // Listbox
